3088e621ca5d8ef4c13ca825074d1db026f6ebbc
BilalENEOUISSER <bilal@digitaleaders.ma>
2022-05-10 17:15:00 +0100

    Ajouter

---
+++ js/scrollEvent.js
@@ -0,0 +1,13 @@
+function scrolf1() {
+    var left1 = document.querySelector(".mv1");
+    left1.scrollBy(500, 0 )
+
+    
+
+}
+
+function scrolr1(){
+
+    var right1 = document.querySelector(".mv1");
+    right1.scrollBy(-500, 0)
+}
---
+++ js/sliderIcon.js
@@ -0,0 +1,13 @@
+function scrolllf() {
+    var left = document.querySelector(".scroll-images1");
+    left.scrollBy(400, 0 )
+
+    
+
+}
+
+function scrollr(){
+
+    var right = document.querySelector(".scroll-images1");
+    right.scrollBy(-400, 0)
+}
